45617eb0b35d3ab753d8124f0539f30522f5a1d0
Heloise Taillet <heloise96@hotmail.com>
2019-01-13 13:58:59 -0500

    all tests pass. no bonus

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
-class Student 
+require 'pry'
+class Student < User
+  def initialize
+    @knowledge = []
+  end
 
+  def learn(knowledge)
+    @knowledge << knowledge
+  end
+
+  def knowledge
+    @knowledge
+  end
 end
